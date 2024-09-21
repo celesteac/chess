984cf367f4de10cb78049ba262b8a129ac435379
celesteac <lestaac73@gmail.com>
2024-09-20 23:11:47 -0600

    restructured pawn move calculator file and fixed move-2 spaces bug

--- shared/src/main/java/chess/pieceMovesCalculator/PawnMovesCalculator.java
+++ shared/src/main/java/chess/pieceMovesCalculator/PawnMovesCalculator.java
@@ -9,66 +9,66 @@ public class PawnMovesCalculator extends PieceMovesCalculator {
         super(board, position, color);
     }
 
-    private int rowIfBlack(int j){
-        return switch (j){
-            case 0 -> row + 1;  //diagonals
-            case 1 -> row + 1;  //diagonals
-            case 2 -> row + 1;  //1 ahead
-            case 3 -> row + 2;  //2 ahead
+    private int getRowIfWhite(int j){
+        return switch (j) {
+            case 0 -> row -1;  //diagonals
+            case 1 -> row -1;  //diagonals
+            case 2 -> row -1;  //forward 1
+            case 3 -> row -2;  //forward 2
             default -> row;
         };
     }
 
-    private int rowIfWhite(int j){
-        return switch (j){
-            case 0 -> row - 1;  //diagonals
-            case 1 -> row - 1;  //diagonals
-            case 2 -> row - 1;  //1 ahead
-            case 3 -> row - 2;  //2 ahead
+    private int getRowIfBlack(int j){
+        return switch (j) {
+            case 0 -> row +1;  //diagonals
+            case 1 -> row +1;  //diagonals
+            case 2 -> row +1;  //forward 1
+            case 3 -> row +2;  //forward 2
             default -> row;
         };
     }
 
+    private int getColumn(int j){
+        return switch (j){
+            case 0 -> col -1;  //diagonals
+            case 1 -> col +1;  //diagonals
+            case 2 -> col;     //forward 1
+            case 3 -> col;     //forward 2
+            default -> col;
+        };
+    }
 
     public Collection<ChessMove> calculateLegalMoves(){
 
-        int possibilities = 3;
-        if(position.getRow() == 2 || position.getRow() == 7){
-            possibilities++;
-        }
+        int possibleMoves =
+                (color == ChessGame.TeamColor.BLACK && row == 1)    //if is black and first move
+                || (color == ChessGame.TeamColor.WHITE && row == 6) //or if is white and first move
+                ? 4 : 3; //possible moves is 4, otherwise 3
 
-        for(int j = 0; j < possibilities; j++){ // checks 3 or 4 possible moves
+        for(int j = 0; j < possibleMoves; j++) {
 
-            int tempRow = (color == ChessGame.TeamColor.WHITE) ? rowIfWhite(j) : rowIfBlack(j);
-
-            int tempCol = switch (j){
-                case 0 -> col - 1; //diagonals
-                case 1 -> col + 1; //diagonals
-                case 2 -> col;     //1 ahead
-                case 3 -> col;     //2 ahead
-                default -> col;
-            };
+            int tempRow = (color == ChessGame.TeamColor.BLACK) ? getRowIfBlack(j) : getRowIfWhite(j);
+            int tempCol = getColumn(j);
 
             tempRow = position.convertRowToChessIndices(tempRow);
             tempCol = position.convertColToChessIndices(tempCol);
             ChessPosition tempPosition = new ChessPosition(tempRow, tempCol);
 
-            if(j > 1){ // forward motions
-                if(checkBounds(tempRow, tempCol)){
-                    if(checkEmpty(tempPosition)){
+
+            if (checkBounds(tempRow, tempCol)) {
+                if (j > 1) { //cases forward 1 or 2 spaces
+                    if (checkEmpty(tempPosition)) {
                         addMove(tempPosition);
                     }
+                    else if(j == 2) break; //piece blocking move 2 action
                 }
-            }
-
-            if(j < 2){ //diagonals
-                if(checkBounds(tempRow, tempCol)) {
+                else { //diagonals
                     if (!checkEmpty(tempPosition) && checkEnemy(tempPosition)) {
                         addMove(tempPosition);
                     }
                 }
             }
-
         }
 
 
