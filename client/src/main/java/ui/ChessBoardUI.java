f1992f4e79d2e41c6c4046d74675eefec6da6716
celesteac <lestaac73@gmail.com>
2024-11-14 21:27:10 -0700

    draws part of board

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -7,14 +7,35 @@ import java.io.PrintStream;
 public class ChessBoardUI {
     private final PrintStream out = System.out;
     private final ChessBoard board;
+    private final int BOARD_SIZE_IN_SQUARES = 8;
+    private final int BOARDER_SIZE_IN_SQUARES = BOARD_SIZE_IN_SQUARES + 2;
 
     public ChessBoardUI(ChessBoard board){
         this.board = board;
     }
 
     public void drawBoard(){
-//        setGray();
+        drawTopBoarder();
         out.print(board.toString());
+    }
+
+    private void drawTopBoarder(){
+        out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+        out.print(EscapeSequences.SET_TEXT_COLOR_BLACK);
+        for(int i = 0; i < BOARDER_SIZE_IN_SQUARES; i++){
+            out.print(EscapeSequences.EMPTY);
+        }
+        setDefaultColors();
+        out.println();
+    }
+
+    private void drawRows(){
+
+    }
+
+    private void setDefaultColors(){
+        out.print(EscapeSequences.SET_TEXT_COLOR_BLUE);
+        out.print(EscapeSequences.RESET_BG_COLOR);
     }
 
     private void setGray(){
