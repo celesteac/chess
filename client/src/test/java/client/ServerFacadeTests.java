1e74d155f30b04234b6bd474001f8e92fc49b773
celesteac <lestaac73@gmail.com>
2024-11-18 15:08:53 -0700

    wrote more server FAcade tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,8 +1,14 @@
 package client;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.AuthData;
 import org.junit.jupiter.api.*;
+import requestresponsetypes.GameDetails;
+import server.ErrorResponse;
 import server.Server;
+
+import java.util.ArrayList;
 
 
 public class ServerFacadeTests {
@@ -37,8 +43,14 @@ public class ServerFacadeTests {
     }
 
     @Test
-    public void registerTestBad() {
+    public void registerBadRequest() {
         Assertions.assertThrows(ResponseException.class, ()->serverFacade.register("celeste", null, "piano@email.com"));
+    }
+
+    @Test
+    public void registerTwiceSameUser(){
+        serverFacade.register("celeste", "secret", "piano@email.com");
+        Assertions.assertThrows(ResponseException.class, ()-> serverFacade.register("celeste", "other", "piano@email.com"));
     }
 
     @Test
@@ -58,16 +70,64 @@ public class ServerFacadeTests {
         serverFacade.register("celeste", "secret", "piano@email.com");
         AuthData auth = serverFacade.login("celeste", "secret");
         serverFacade.logout(auth.authToken());
-//        Assertions.assertDoesNotThrow(ResponseException.class, ()-> );
+        Exception ex = Assertions.assertThrows(ResponseException.class, ()-> serverFacade.register("celeste", "secret", "piano@email.com"));
     }
+
+    @Test
+    public void logoutTestBad(){
+        serverFacade.register("celeste", "secret", "piano@email.com");
+        AuthData auth = serverFacade.login("celeste", "secret");
+        Exception ex = Assertions.assertThrows(ResponseException.class, ()-> serverFacade.logout(null));
+    }
+
+    //how to test logout effectively?
+    //tried to get message -> should do that for more of the tests
 
     @Test
     public void createTestGood(){
         serverFacade.register("celeste", "secret", "piano@email.com");
         AuthData auth = serverFacade.login("celeste", "secret");
         serverFacade.createGame("new game", auth.authToken());
-//        serverFacade.
+        serverFacade.createGame("new game", auth.authToken());
+        ArrayList<GameDetails> games =  serverFacade.listGames(auth.authToken());
+        Assertions.assertEquals(2, games.size());
     }
+
+    @Test
+    public void createTestBad(){
+        serverFacade.register("celeste", "secret", "piano@email.com");
+        AuthData auth = serverFacade.login("celeste", "secret");
+        Assertions.assertThrows(ResponseException.class, ()-> serverFacade.createGame(null, auth.authToken()));
+    }
+
+    @Test
+    public void listBad(){
+        serverFacade.register("celeste", "secret", "piano@email.com");
+        AuthData auth = serverFacade.login("celeste", "secret");
+        serverFacade.createGame("new game", auth.authToken());
+        Assertions.assertThrows(ResponseException.class, ()-> serverFacade.listGames(null));
+    }
+
+    @Test
+    public void joinGood(){
+        serverFacade.register("celeste", "secret", "piano@email.com");
+        AuthData auth = serverFacade.login("celeste", "secret");
+        serverFacade.createGame("new game", auth.authToken());
+        ArrayList<GameDetails> games = serverFacade.listGames(auth.authToken());
+        serverFacade.joinGame(games.getFirst().gameID(), ChessGame.TeamColor.WHITE, auth.authToken());
+        ArrayList<GameDetails> newGames = serverFacade.listGames(auth.authToken());
+        Assertions.assertEquals("celeste", newGames.getFirst().whiteUsername());
+    }
+
+    @Test
+    public void joinBad(){
+        serverFacade.register("celeste", "secret", "piano@email.com");
+        AuthData auth = serverFacade.login("celeste", "secret");
+        serverFacade.createGame("new game", auth.authToken());
+        ArrayList<GameDetails> games = serverFacade.listGames(auth.authToken());
+        Assertions.assertThrows(ResponseException.class, ()-> serverFacade.joinGame(games.getFirst().gameID(), null, auth.authToken()));
+    }
+
 
 
 }
